5e9a031ed64250e3701ce60e9ff8ea39c391e111
sieger <oluwaseun.v@yahoo.com>
2020-03-23 00:41:58 +0100

    js task

---
+++ index.js.js
@@ -0,0 +1,7 @@
+var name = "ogunyomi-adekoya victor"
+console.log(name);
+var courses=["html","css","javascript","python"]
+console.log(courses);
+var i; for(i=0;i<=200;i++) { if(i % 2 == 0)
+    console.log(i);
+}
